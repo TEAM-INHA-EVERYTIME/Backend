777149580853e8501eeb0d943495949221031324
김민기 <mocodoco@naver.com>
2024-11-06 18:48:59 +0900

    spring boot 개발 환경 세팅 완료

---
+++ src/main/java/com/example/inhating/InhatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.inhating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InhatingApplication.class, args);
+    }
+
+}

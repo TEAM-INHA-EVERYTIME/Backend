e3c8b5fd3f45ef9aad817c3ea96248075dbdc319
박근채 <pgc0419@gmail.com>
2024-11-13 14:30:20 +0900

    feat: user테이블 controller 구현

--- src/main/java/inhating/user/controller/UserController.java
+++ src/main/java/inhating/user/controller/UserController.java
@@ -1,3 +1,47 @@
 package inhating.user.controller;
 
-import inhating.user.*;
+import inhating.user.domain.*;
+import inhating.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserAddRequest request) {
+        User createdUser = userService.createUser(request);
+        URI location = URI.create("/api/users/" + createdUser.getId());
+        return ResponseEntity.created(location).body(createdUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserModifyRequest request
+    ) {
+        User updatedUser = userService.updateUser(id, request);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.findUserById(id);
+        return ResponseEntity.ok(user);
+    }
+}

6e41850b78e24b6882267be32071140ae0858523
박근채 <pgc0419@gmail.com>
2024-12-05 17:52:08 +0900

    Refactor: UserReponse 주석 해제

--- src/main/java/inhating/user/domain/UserRepository.java
+++ src/main/java/inhating/user/domain/UserRepository.java
@@ -3,7 +3,7 @@ package inhating.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-//로그인 시 userId 매핑을 위한 클래스
+//로그인 시 userId 매핑을 위한 JPA 인터페이스
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
 }
--- src/main/java/inhating/user/domain/UserResponse.java
+++ src/main/java/inhating/user/domain/UserResponse.java
@@ -1,32 +1,33 @@
-//package inhating.user.domain;
-//
-//import lombok.Getter;
-//
-//import java.time.LocalDateTime;
-//
-//@Getter
-//public class UserResponse {
-//    private Long id;
-//    private String userId;
-//    private String name;
-//    private String nickName;
-//    private Boolean gender;
-//    private LocalDateTime birth;
-//    private String major;
-//    private String MBTI;
-//    private String introduce;
-//    private int taxiTemperature;
-//
-//    public UserResponse(User user) {
-//        this.id = user.getId();
-//        this.userId = user.getUserId();
-//        this.name = user.getName();
-//        this.nickName = user.getNickName();
-//        this.gender = user.getGender();
-//        this.birth = user.getBirth();
-//        this.major = user.getMajor();
-//        this.MBTI = user.getMBTI();
-//        this.introduce = user.getIntroduce();
-//        this.taxiTemperature = user.getTaxiTemperature();
-//    }
-//}
+// DTO 역할로써 클라이언트에게 정보 제공하는 클래스
+package inhating.user.domain;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserResponse {
+    private Long id;
+    private String userId;
+    private String name;
+    private String nickName;
+    private Boolean gender;
+    private LocalDateTime birth;
+    private String major;
+    private String MBTI;
+    private String introduce;
+    private int taxiTemperature;
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.userId = user.getUserId();
+        this.name = user.getName();
+        this.nickName = user.getNickName();
+        this.gender = user.getGender();
+        this.birth = user.getBirth();
+        this.major = user.getMajor();
+        this.MBTI = user.getMBTI();
+        this.introduce = user.getIntroduce();
+        this.taxiTemperature = user.getTaxiTemperature();
+    }
+}
